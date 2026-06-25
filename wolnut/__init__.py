31fc010cc4e7bbd590e6ff6904616b706f11fca2
barecool <barecool@gmail.com>
2026-06-25 16:44:22 -0500

    Change version to 0.2.0 and add module description
    Updated version number and added module description.

--- wolnut/__init__.py
+++ wolnut/__init__.py
@@ -1,4 +1,5 @@
-__version__ = "1.0.0"
-from wolnut.cli import main  # re-export
+"""
+WolNut - A clean utility parsing cross-platform UPS structures to command machine Wake-on-LAN states.
+"""
 
-__all__ = ("main",)
+__version__ = "0.2.0"
