b63d5e87c0b53156888099ad3a9a84bfb0221453
barecool <barecool@gmail.com>
2026-06-25 16:37:18 -0500

    Refactor main function and add logging

--- wolnut/__main__.py
+++ wolnut/__main__.py
@@ -1,6 +1,36 @@
+import argparse
 import sys
+import logging
+from wolnut.config import load_config
+from wolnut.monitors import WolNutMonitor
 
-from wolnut.cli import wolnut
+def main():
+    parser = argparse.ArgumentParser(description="WolNut: UPS-triggered Wake-on-LAN client utility.")
+    parser.add_argument(
+        "--config", 
+        default="/config/config.yaml", 
+        help="Path to the application configuration file"
+    )
+    args = parser.parse_args()
+
+    # Setup application logs
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        handlers=[logging.StreamHandler(sys.stdout)]
+    )
+    logger = logging.getLogger("wolnut")
+
+    try:
+        logger.info(f"Loading configuration from: {args.config}")
+        config = load_config(args.config)
+        
+        monitor = WolNutMonitor(config)
+        monitor.start(interval_sec=10)
+        
+    except Exception as e:
+        logger.critical(f"Application failed to initialize: {e}", exc_info=True)
+        sys.exit(1)
 
 if __name__ == "__main__":
-    wolnut()
+    main()
